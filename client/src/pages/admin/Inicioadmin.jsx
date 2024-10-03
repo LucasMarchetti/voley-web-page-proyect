ddc3406e2a96dd035583416af141df6c583d6893
Valentino Marchetti <valentinomarchettti@gmail.com>
2024-10-03 02:52:20 -0300

    add NewCards,AllCards

--- client/src/pages/admin/Inicioadmin.jsx
+++ client/src/pages/admin/Inicioadmin.jsx
@@ -1,16 +1,37 @@
 import CategorySelect from "../../components/admin/CategorySelect";
 import Sidebar from "../../components/admin/Sidebar";
 import RoundRobinCard from "../../components/admin/RoundRobinCard";
+import NewPermissionCard from "../../components/admin/NewPermissionCard";
+import AllPermissionsCard from "../../components/admin/AllPermissionsCard";
+import NewStadiumCard from "../../components/admin/NewStadiumCard";
+import AllStadiumsCard from "../../components/admin/AllStadiumsCard";
 import PersonalizedRoundRobinCard from "../../components/admin/PersonalizedRoundRobinCard";
 import "./styles.css"
 
 export default function federacionCatamarca () {
 
     return (
-        <div className="admin-page">
-            <Sidebar/>
-            <RoundRobinCard/>
-            <PersonalizedRoundRobinCard/>
-        </div>
+        <body>
+            <div className="admin-page">
+                <Sidebar/>
+                <div className="contenedor-padre">
+                    <div className="contenedor-hijo">
+                        <RoundRobinCard/>
+                        <PersonalizedRoundRobinCard/>
+                    </div>
+                    <div className="contenedor-hijo">
+                        <NewPermissionCard/>
+                        <AllPermissionsCard/>
+                    </div>
+                    <div className="contenedor-hijo">
+                        <NewStadiumCard/>
+                        <AllStadiumsCard/>
+                    </div>
+
+                   
+                </div>
+            </div>
+        </body>
+        
     )
 }
