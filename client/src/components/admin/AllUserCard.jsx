6f19f7e3eee906a1bd33b23ba7a8e36e85175911
Felip27 <luisfelipediaz2727@gmail.com>
2024-10-03 17:22:29 -0300

    add cards

---
+++ client/src/components/admin/AllUserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/AllUserCard.css';
+
+const AllUserCard = () => {
+    return (
+    <div className="container-AllUserCard">
+        <h3 className="subtitle-AllUserCard">Administrar</h3>
+        <h1 className="title-AllUserCard">Todos los usuarios</h1>    
+        </div>
+    );
+};
+
+
+export default AllUserCard;
