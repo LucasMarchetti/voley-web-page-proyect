88b0aa2bbad1cc8ce01772f58beb98f28ffed474
LucasMarchetti <luksmarchetti@gmail.com>
2024-10-03 19:20:11 -0300

    add Contact component

--- client/src/pages/federacionLaRioja.jsx
+++ client/src/pages/federacionLaRioja.jsx
@@ -1,13 +1,31 @@
 //Styles
 import "../components/styles/federacionLaRioja.css"
 
+//components
+import Contact from "../components/Contact"
+import HeaderFederaciones from "../components/HeaderFederaciones"
+import Torneos from "../components/torneos"
+import UltimosPartidos from "../components/UltimosPartidos"
+import NavBarFederaciones from "../components/NavBarFederaciones.jsx"
+
+let LaRioja = {
+    "nombre": "Federación Riojana de Voley",
+    "email": "contacto@voleylarioja.com",
+    "telefono": "+54 380 432 1010",
+    "direccion": "Calle Pelagio B. Luna 678, La Rioja Capital",
+    "logo": require("../multimedia/logoRioja.png")
+  }
 
 export default function federacionLaRioja () {
 
 
     return (
         <div>
-            Soy federacionLaRioja
+            <NavBarFederaciones />
+            <HeaderFederaciones Provincia={LaRioja} />
+            <UltimosPartidos />
+            <Torneos />
+            <Contact Provincia={LaRioja}/>
         </div>
     )
 }
--- client/src/pages/federacionTucuman.jsx
+++ client/src/pages/federacionTucuman.jsx
@@ -1,13 +1,30 @@
 //Styles
 import "../components/styles/federacionTucuman.css"
 
+//components
+import Contact from "../components/Contact"
+import HeaderFederaciones from "../components/HeaderFederaciones"
+import Torneos from "../components/torneos"
+import UltimosPartidos from "../components/UltimosPartidos"
+import NavBarFederaciones from "../components/NavBarFederaciones.jsx"
+
+let Tucuman = {
+    "nombre": "Federación Tucumana de Voley",
+    "email": "fedevoley@tucuman.gov.ar",
+    "telefono": "+54 381 465 7890",
+    "direccion": "Av. Aconquija 789, San Miguel de Tucumán",
+    "logo": require("../multimedia/logoTucuman.webp")
+  }
 
 export default function federacionTucuman () {
 
-
     return (
         <div>
-            Soy federacionTucuman
+            <NavBarFederaciones />
+            <HeaderFederaciones Provincia={Tucuman} />
+            <UltimosPartidos />
+            <Torneos />
+            <Contact  Provincia={Tucuman}/>
         </div>
     )
 }
